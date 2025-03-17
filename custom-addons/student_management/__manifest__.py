0e02303575a246e362c3a7e21e9f86080921b4f0
TuPT2 <tupt2@bap.jp>
2025-03-17 14:20:39 +0700

    Initial commit

---
+++ custom-addons/student_management/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Student Management",
+
+    'summary': "Test Summary",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Thanh Tu",
+    'website': "https://www.fb.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Website',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/student_views.xml',
+        'security/student_security.xml',
+        'security/ir.model.access.csv',
+        'views/hr_payroll_menu.xml',
+        'views/hr_payslip_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
+
