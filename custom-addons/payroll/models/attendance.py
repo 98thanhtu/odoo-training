3f596e53e87c346110734c077a7b0fa349333c2a
TuPT2 <tupt2@bap.jp>
2025-03-21 14:44:45 +0700

    Add Compute Salary

--- custom-addons/payroll/models/attendance.py
+++ custom-addons/payroll/models/attendance.py
@@ -1,34 +1,42 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class PayrollAttendance(models.Model):
+class Attendance(models.Model):
     _name = 'payroll.attendance'
-    _description = 'Payroll Attendance'
+    _description = 'Attendance'
 
     employee_id = fields.Many2one('payroll.employee', string='Employee', required=True)
     check_in = fields.Datetime(string='Check In', required=True)
     check_out = fields.Datetime(string='Check Out', required=True)
-    hours_worked = fields.Float(string='Hours Worked', compute='_compute_hours_worked', store=True)
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('confirmed', 'Confirmed')
-    ], string='Status', default='draft')
+    worked_hours = fields.Float(string='Worked Hours', compute='_compute_worked_hours', store=True)
 
     @api.depends('check_in', 'check_out')
-    def _compute_hours_worked(self):
+    def _compute_worked_hours(self):
         for record in self:
             if record.check_in and record.check_out:
-                duration = record.check_out - record.check_in
-                record.hours_worked = duration.total_seconds() / 3600  # Chuyển thành giờ
+                delta = record.check_out - record.check_in
+                record.worked_hours = delta.total_seconds() / 3600
+            else:
+                record.worked_hours = 0.0
 
-    @api.model
-    def create(self, vals):
-        attendance = super(PayrollAttendance, self).create(vals)
-        attendance.state = 'confirmed'
-        return attendance
+    # @api.model
+    # def create(self, vals):
+    #     attendance = super(PayrollAttendance, self).create(vals)
 
-    def unlink(self):
+    #     payslip = self.env['payroll.payslip'].search([
+    #         ('employee_id', '=', attendance.employee_id.id),
+    #         ('month', '=', str(attendance.check_in.month)),
+    #         ('year', '=', attendance.check_in.year)
+    #     ], limit=1)
+
+    #     if payslip:
+    #         payslip._compute_total_hours()
+    #         payslip._compute_total_salary()
+
+    #     return attendance
+
+    @api.constrains('check_in', 'check_out')
+    def _check_check_in_out(self):
         for record in self:
-            if record.state == 'confirmed' and not self.env.user.has_group('payroll.group_hr_manager'):
-                raise ValidationError("You are not allowed to delete confirmed attendance records.")
-        return super(PayrollAttendance, self).unlink()
+            if record.check_in and record.check_out and record.check_in >= record.check_out:
+                raise ValidationError("Check In must be before Check Out.")
