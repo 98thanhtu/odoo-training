86304fe4d3c67b65387db5677ffc023f738615a7
TuPT2 <tupt2@bap.jp>
2025-05-19 15:00:48 +0700

    Fix Update Sprint

--- custom-addons/project_management/models/sprint.py
+++ custom-addons/project_management/models/sprint.py
@@ -22,25 +22,6 @@ class Sprint(models.Model):
         record.code = f"SPR{record.id:05d}"
         return record
 
-    @api.constrains('project_id')
-    def _check_project_closed(self):
-        for record in self:
-            project = record.project_id
-
-            sprints = self.env['pr.sprint'].search([('project_id', '=', project.id)])
-            tasks = self.env['pr.task'].search([('project_id', '=', project.id)])
-
-            if sprints:
-                open_sprints = sprints.filtered(lambda s: s.state != 'close')
-                if open_sprints:
-                    raise ValidationError("Cannot create project close request because there are unclosed sprints.")
-
-            if tasks:
-                unfinished_tasks = tasks.filtered(lambda t: t.state != 'done')
-                if unfinished_tasks:
-                    raise ValidationError("Cannot create project close request because there are unfinished tasks.")
-
-
     def write(self, vals):
         if 'status' in vals and vals['status'] == 'open':
             for sprint in self:
